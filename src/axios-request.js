9d5a86e76f4488cb15929da0e74d183b7a06a0ca
Asad Bukhari <57019121+asdbukhari@users.noreply.github.com>
2022-12-14 22:06:13 +0500

    update

--- src/axios-request.js
+++ src/axios-request.js
@@ -1,7 +1,7 @@
 import Axios from "axios";
 
 const instance = Axios.create({
-	baseURL: "http://192.168.43.47:8000/api/" /*local server*/,
+	baseURL: "http://localhost:8000/api/" /*local server*/,
 	// baseURL: 'http://192.168.10.14:8000/api/' /*local server*/
 });
 
